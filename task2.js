54c9ffd6eb2ed072d671c5bba35683465a4f7e17
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-21 10:33:22 +0300

    multi 2

--- task2.js
+++ task2.js
@@ -1,11 +1,15 @@
 function mul(...args) {
-    var m = 1
-    args.forEach(x => m = m * x)
-    return m
+    //var m = 1
+    //args.forEach(x => m = m * x)
+    var result = args.reduce(function(m, current) {
+        return m * current;
+      }, 1);
+    return result
 }
 
 console.log(mul(2, 3, 4));
 console.log(mul(4, 3, 4));
+//console.log(mul(2)(3)(4));
 /*
  
 console.log(mul(2)(3)(4)); // output : 24
