9ab094e1408335ffd6660f5b987eaf14755bfcbb
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-21 07:32:07 +0300

    правки

--- server1.js
+++ server1.js
@@ -8,24 +8,20 @@ var dateFormat = require('dateformat');
 var ts_hms = Date.now();
 //var out = "Time now " + dateFormat(ts_hms, "isoDateTime")  //.format("%Y-%m-%d %H:%M:%S")
 function accept(req, res) {
-
-  console.log(req); // запусти сервер и попробуй зайти на http://127.0.0.1:8080/testtesttest
-
   res.writeHead(200, {
-    'Content-Type': 'text/plain',
+    'Content-Type': 'text/plain; charset=utf-8',
     'Cache-Control': 'no-cache'
   });
   var route = url.parse(req.url).path
   switch (route) {
-    case "/time": res.end("Time now " + dateFormat(ts_hms, "isoDateTime"));
+    case "/time": res.end("Time now " + dateFormat(ts_hms, "mediumTime"));
       break
     default:
-      res.end("hello")
+      res.end("Привет Andrey")
   }
 
   console.log(url.parse(req.url).path)
-  //res.end(out+"---"+req);
-  //res.end(req.toString())
+
 }
 
 http.createServer(accept).listen(8080);
