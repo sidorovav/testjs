6359cff218c77c82890fe8fecaeb998b6ef26536
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-21 10:39:51 +0300

    anonimus function

--- task2.js
+++ task2.js
@@ -1,4 +1,4 @@
-function mul(...args) {
+function mul1(...args) {
     //var m = 1
     //args.forEach(x => m = m * x)
     var result = args.reduce(function(m, current) {
@@ -6,13 +6,18 @@ function mul(...args) {
       }, 1);
     return result
 }
-
-console.log(mul(2, 3, 4));
-console.log(mul(4, 3, 4));
-//console.log(mul(2)(3)(4));
-/*
+function mul (a) {
+    return function (b) 
+    { 
+      return function (c) 
+      { 
+        return a * b * c;
+      };
+    };
+}
+console.log(mul1(2, 3, 4));
+console.log(mul1(4, 3, 4));
  
 console.log(mul(2)(3)(4)); // output : 24
 console.log(mul(4)(3)(4)); // output : 48
  
-*/
