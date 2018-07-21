af0ef3260443651c652234cd5e4e7c50c5c4e917
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-21 09:24:34 +0300

    slip 1 sec

--- server1.js
+++ server1.js
@@ -3,26 +3,28 @@ var url = require('url');
 var querystring = require('querystring');
 var dateFormat = require('dateformat');
 
-
-// не знаю с ч
-var ts_hms = Date.now();
-//var out = "Time now " + dateFormat(ts_hms, "isoDateTime")  //.format("%Y-%m-%d %H:%M:%S")
 function accept(req, res) {
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8',
     'Cache-Control': 'no-cache'
   });
   var route = url.parse(req.url).path
+  console.log(route)
   switch (route) {
-    case "/time": res.end("Time now " + dateFormat(ts_hms, "mediumTime"));
+    case "/time":
+      var timerId = setTimeout(t1,1000,res);
+      res.write("Ждите ...");
+      
       break
     default:
-      res.end("Привет Андрей! Смотри "+ "<a href='/time'>Время</a>")
+      res.end("Привет Андрей! Смотри " + "<a href='/time'>Время</a>")
   }
-
-  console.log(url.parse(req.url).path)
-
+  function t1(res){
+    console.log("dzin");
+    res.end("Time now " + dateFormat(Date.now(), "mediumTime"))
+  }
 }
+
 
 http.createServer(accept).listen(8080);
 console.log("http://127.0.0.1:8080")
