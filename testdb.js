a7952d16b9a8f5e9cfcabcb135fa62f3007d1ffe
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-23 01:17:14 +0300

    count1

--- testdb.js
+++ testdb.js
@@ -3,29 +3,60 @@ var db = new sqlite3.Database('./DB/db1.db');
 // http://www.sqlitetutorial.net/sqlite-nodejs/query/ 
 
 function prepareDB(db){
-    db.run ("create table if not exists products (id NUMBER,product JSON)");
-    var count = 0;
-    db.each("SELECT count(id) as count FROM products", function(err, row) {count = row.count});
-    if (count >= 0) {
-        var stmt = db.prepare("INSERT INTO products VALUES (?,?)");
-        for (var i = 0; i < 10; i++) {
-            testproduct.id=i,testproduct.product.name="сыр " + i
-            stmt.run(testproduct.id,JSON.stringify(testproduct.product));
-        }
-        stmt.finalize();
-    }    
+    db.run ("create table if not exists products (id INTEGER PRIMARY KEY AUTOINCREMENT,product JSON)");
+    
+    productGetCount(db,function(data){
+        if (data == 0) {
+          //  productInitData(db)
+          console.log("пусто")
+        } else {console.log("есть данные")}
+    })
+    
+  // productInitData(db)
+}
+function productInitData(db){
+    var testproduct ={id:1,product:{name:"сыр",description:"молочный"}};
+    var stmt = db.prepare("INSERT INTO products (product) VALUES (?)");
+    for (var i = 0; i < 10; i++) {
+        testproduct.id=i,testproduct.product.name="сыр " + (i+1)
+        //stmt.run(testproduct.id,JSON.stringify(testproduct.product));
+        stmt.run(JSON.stringify(testproduct.product));
+    }
+    stmt.finalize();    
+}
+function productGetCount(db,callback){
+    var data;
+    db.each("SELECT count(id) as count FROM products", function(err, row) {
+        data =row.count;
+    }, function(){ // calling function when all rows have been pulled
+       // db.close(); //closing connection
+        callback(data); 
+    });
+}
+
+function productDel(db,id){
+    db.exec("delete FROM products where id = " + id, function(err, row) {
+    })
+}
+function productGetById(db,id,callback){
+    var data;
+    db.each("SELECT product FROM products WHERE id = " + id , function(err, row) {
+        data =row;
+    }, function(){ // calling function when all rows have been pulled
+       // db.close(); //closing connection
+        callback(data); 
+    });
 }
 function showData(db){
     db.each("SELECT * FROM products order by id", function(err, row) {
         console.log(row.id + ": " + row.product);
     })
 }
-var testproduct ={id:1,product:{name:"сыр",description:"молочный"}};
 db.serialize(function() {
     prepareDB(db);
-    showData(db)
+    //showData(db);
+    //productDel(db,8);
+    //showData(db);
+    productGetById(db,5, function(data) {console.log(data)});
  });
-
-
-console.log(testproduct); 
 db.close();
