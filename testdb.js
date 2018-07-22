ef60b0ddd4a97c8309db7bdab80bd18c69c37ea1
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-22 08:30:57 +0300

    test db sqllite

--- testdb.js
+++ testdb.js
@@ -1,1 +1,18 @@
-const DB = require('sqlite-crud')('./mydb.db');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('DB/db1.db');
+ 
+db.serialize(function() {
+  //db.run("CREATE TABLE lorem (info TEXT)");
+ 
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+ 
+  db.each("SELECT rowid AS id, info FROM lorem order by info", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+ 
+db.close();
