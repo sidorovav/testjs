f4aeda070dbc5b3233a98ceb40b1aa438d8d8108
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-22 11:24:59 +0300

    db 2

--- testdb.js
+++ testdb.js
@@ -1,21 +1,7 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./DB/db1.db');
 // http://www.sqlitetutorial.net/sqlite-nodejs/query/ 
-/*
-db.serialize(function() {
-  //db.run("CREATE TABLE lorem (info TEXT)");
- 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
- 
-  db.each("SELECT rowid AS id, info FROM lorem order by info", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});
-*/
+
 function prepareDB(db){
     db.run ("create table if not exists products (id NUMBER,product JSON)");
     var count = 0;
@@ -29,13 +15,15 @@ function prepareDB(db){
         stmt.finalize();
     }    
 }
-
-var testproduct ={id:1,product:{name:"сыр",description:"молочный"}};
-db.serialize(function() {
-    prepareDB(db);
+function showData(db){
     db.each("SELECT * FROM products order by id", function(err, row) {
         console.log(row.id + ": " + row.product);
     })
+}
+var testproduct ={id:1,product:{name:"сыр",description:"молочный"}};
+db.serialize(function() {
+    prepareDB(db);
+    showData(db)
  });
 
 
