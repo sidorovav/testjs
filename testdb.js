28b01080096f5b7bf6a8b4dc3ada58cca4e2a48b
Aleksandr Sidorov <sidorovav@gmail.com>
2018-07-22 09:49:46 +0300

    наполнение продуктов

--- testdb.js
+++ testdb.js
@@ -1,6 +1,7 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('DB/db1.db');
- 
+var db = new sqlite3.Database('./DB/db1.db');
+// http://www.sqlitetutorial.net/sqlite-nodejs/query/ 
+/*
 db.serialize(function() {
   //db.run("CREATE TABLE lorem (info TEXT)");
  
@@ -14,5 +15,17 @@ db.serialize(function() {
       console.log(row.id + ": " + row.info);
   });
 });
- 
+*/
+var testproduct ={id:1,product:{name:"сыр",description:"молочный"}};
+db.serialize(function() {
+    db.run ("create table if not exists products (id NUMBER,product JSON)");
+    var stmt = db.prepare("INSERT INTO products VALUES (?,?)");
+        for (var i = 0; i < 10; i++) {
+            testproduct.id=i,testproduct.product.name="сыр " + i
+            stmt.run(testproduct.id,JSON.stringify(testproduct.product));
+        }
+        stmt.finalize();
+ });
+
+console.log(testproduct); 
 db.close();
